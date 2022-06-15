dde8fcc36dcc3bf56d67526ff5424c90b44ca8be
John Rhanzel <rhanzel1016@gmail.com>
2022-06-15 20:31:01 +0800

    Create passFunctionArgs.js

---
+++ Basics of JavaScript/passFunctionArgs.js
@@ -0,0 +1,10 @@
+//passing values to functions with arguments
+
+//parameters are variables that acts as placeholders for the values that are to be input to a function when called
+
+function functionWithArgs(param1, param2){
+    console.log(param1 + param2); //this will add the two parameters
+  }
+  
+  functionWithArgs(1,2); //the output is 3
+  functionWithArgs(7,9); //the output is 16
