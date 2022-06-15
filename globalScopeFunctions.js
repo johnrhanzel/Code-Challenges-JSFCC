88b364f8d37e39d0f3dbcfaf841da3846275257e
John Rhanzel <rhanzel1016@gmail.com>
2022-06-15 21:13:45 +0800

    Create globalScopeFunctions.js

---
+++ globalScopeFunctions.js
@@ -0,0 +1,26 @@
+//global scope and functions
+//scope refers to the visibility of variables
+//variables defined outside of a function block have Global scope
+// variable declared without let or const are automatically in the global scope
+
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
