2ca7e4e8ed43f14feed48df413d5cb9ac5b17351
John Rhanzel <rhanzel1016@gmail.com>
2022-06-15 22:29:28 +0800

    Create assignmentReturnValue.js

---
+++ Basics of JavaScript/assignmentReturnValue.js
@@ -0,0 +1,11 @@
+//assignmet with a returned value
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7); //call the  function with an argument of 7 and assign its return value to variable processed
