472df426ac3dcc9f958e65f0825701dd6844a6f5
John Rhanzel <rhanzel1016@gmail.com>
2022-06-15 21:01:59 +0800

    Create returnFunctionArgs.js

---
+++ Basics of JavaScript/returnFunctionArgs.js
@@ -0,0 +1,8 @@
+//return value from a function using return
+
+function timesFive(num){
+    return num * 5; //use return to send a value back out of a function
+  }
+  timesFive(5); //the output is 25
+  timesFive(2); //the output is 10
+  timesFive(0); //the output is 0
