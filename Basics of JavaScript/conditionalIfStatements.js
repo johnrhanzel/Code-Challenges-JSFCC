bae3476c2f104f0a6cc773a5db3f550f5dbc958a
John Rhanzel <rhanzel1016@gmail.com>
2022-06-16 21:42:45 +0800

    Create conditionalIfStatements.js

---
+++ Basics of JavaScript/conditionalIfStatements.js
@@ -0,0 +1,15 @@
+//use conditinal logic with if statements
+
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+    if(wasThatTrue){ //if wasThatTrue it will print
+      return "Yes, that was true";
+    }
+    return "No, that was false"; //otherwise it will print this statement
+  
+    // Only change code above this line
+  
+  }
+  
+  trueOrFalse(true);
+  trueOrFalse(false);
