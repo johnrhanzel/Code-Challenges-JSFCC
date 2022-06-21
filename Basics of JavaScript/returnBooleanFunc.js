fd54f4f2daa35c9a1d0df8d5ef9052a02c125316
John Rhanzel <rhanzel1016@gmail.com>
2022-06-21 23:10:16 +0800

    Create returnBooleanFunc.js

---
+++ Basics of JavaScript/returnBooleanFunc.js
@@ -0,0 +1,9 @@
+//returning boolean values from functions
+
+function isLess(a, b) {
+    // Only change code below this line
+    return a < b; //remove if else and change to return a < b
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
