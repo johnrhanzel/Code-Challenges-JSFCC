695b8f6fe08b321e73c7e5b89af45b5ae12f9c7c
John Rhanzel <rhanzel1016@gmail.com>
2022-06-22 13:22:26 +0800

    Create returnEarlyPattern.js

---
+++ Basics of JavaScript/returnEarlyPattern.js
@@ -0,0 +1,14 @@
+//return early pattern for functions
+
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+    if (a < 0 || b < 0) return undefined; //undefined is a keyword not a string
+  
+  
+    // Only change code above this line
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  abTest(2,2);
