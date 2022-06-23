6046e8a21af307ff7dfc09c3fafba44321e0d858
John Rhanzel <rhanzel1016@gmail.com>
2022-06-23 13:52:20 +0800

    Create objectBracketNotation.js

---
+++ Basics of JavaScript/objectBracketNotation.js
@@ -0,0 +1,13 @@
+//accessing object properties using bracket notation
+//you can use bracket notation if an object has a space in its name
+
+// Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj["an entree"];   // Change this line
+  const drinkValue = testObj["the drink"];    // Change this line
