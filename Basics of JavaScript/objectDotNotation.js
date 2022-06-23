bd2d6d48aa2478339d326f2fa418d6d35676003a
John Rhanzel <rhanzel1016@gmail.com>
2022-06-23 13:09:40 +0800

    Create objectDotNotation.js

---
+++ Basics of JavaScript/objectDotNotation.js
@@ -0,0 +1,13 @@
+//accessing object properties with dot notation
+//two ways to access the propertes of object: dot notation(.) and bracket notation ([]), which is similar to an array
+
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
