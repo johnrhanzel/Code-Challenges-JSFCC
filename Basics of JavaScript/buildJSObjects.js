f819cc774bf8243d66804bb8f2c78749afde2b7d
John Rhanzel <rhanzel1016@gmail.com>
2022-06-23 12:55:53 +0800

    Create buildJSObjects.js

---
+++ Basics of JavaScript/buildJSObjects.js
@@ -0,0 +1,12 @@
+//build javascrip objects
+//objects are similar to arrays, except that you access data in objects using properties
+
+const myDog = {
+    // Only change code below this line
+    "name": "Joe", //joe is a string
+    legs: 2, //legs and tails are numbers
+    tails: 1,
+    "friends": ["Mica", "Han", "Jessica"] //array
+  
+    // Only change code above this line
+  };
